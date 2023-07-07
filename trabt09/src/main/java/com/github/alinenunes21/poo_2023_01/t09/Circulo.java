7db308eeed1b689dd2db3872d638d1353a5c20f2
alinenunes21 <111403340+alinenunes21@users.noreply.github.com>
2023-07-07 20:40:12 -0300

    trabalho t09

---
+++ trabt09/src/main/java/com/github/alinenunes21/poo_2023_01/t09/Circulo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Circulo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o valor do raio: ");
+        double raio = scanner.nextDouble();
+
+        // Calcula a circunferência
+        double circunferencia = 2 * Math.PI * raio;
+
+        System.out.println("A circunferência do círculo é: " + circunferencia);
+    }
+}
+
+
+
---
+++ trabt09/src/main/java/com/github/alinenunes21/poo_2023_01/t09/Pessoa.java
@@ -0,0 +1,15 @@
+package com.github.alinenunes21.poo_2023_01.t09;
+
+public class Pessoa {
+    class Habito {
+        private String nome;
+
+        public Habito(String nome) {
+            this.nome = nome;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+    }
+}
