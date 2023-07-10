c62d7f9dd250fcc6e2b1986b19840bc4fede2a3e
Alinenunes21 <aline.aline2@discente.ufg.br>
2023-07-10 16:32:47 -0300

    t17 enviado novamente

---
+++ src/main/java/com/github/alinenunes21/poo_2023_01/t17/Monitor.java
@@ -0,0 +1,6 @@
+package com.github.alinenunes21.poo_2023_01.t17;
+
+public class Monitor {
+    public void tamanhoDoMonitor() {
+    }
+}
---
+++ src/main/java/com/github/alinenunes21/poo_2023_01/t17/Teclado.java
@@ -0,0 +1,7 @@
+package com.github.alinenunes21.poo_2023_01.t17;
+
+public class Teclado {
+    char tamanho;
+    char telasAdicionais;
+
+}
