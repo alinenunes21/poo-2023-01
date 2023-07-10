90f74668c32a16b46e9c79e978757a1b005aca5d
alinenunes21 <111403340+alinenunes21@users.noreply.github.com>
2023-07-10 14:48:52 -0300

    Delete src/main/java/com/github/alinenunes21/poo_2023_01/t10 directory

--- src/main/java/com/github/alinenunes21/poo_2023_01/t10/Bicicleta.java
+++
@@ -1,9 +0,0 @@
-public class Bicicleta {
-
-        private String marca;
-        private String modelo;
-        private String cor;
-        private String tamanho;
-        private String numeroSerie;
-    }
-
