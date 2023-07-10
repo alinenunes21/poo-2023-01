589b6d5cc85105192165d035bd562754e7dc2ae2
Alinenunes21 <aline.aline2@discente.ufg.br>
2023-07-10 18:13:43 -0300

    t10 enviado novamente

---
+++ src/main/java/com/github/alinenunes21/poo_2023_01/t10/Bicicleta.java
@@ -0,0 +1,9 @@
+public class Bicicleta {
+
+        private String marca;
+        private String modelo;
+        private String cor;
+        private String tamanho;
+        private String numeroSerie;
+    }
+
