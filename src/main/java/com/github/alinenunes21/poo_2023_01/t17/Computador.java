496593c2dea0ec25b38e9940d1aed476e6460ce7
Alinenunes21 <aline.aline2@discente.ufg.br>
2023-07-19 19:44:48 -0300

    arrumando repositírio

---
+++ src/main/java/com/github/alinenunes21/poo_2023_01/t17/Computador.java
@@ -0,0 +1,8 @@
+package com.github.alinenunes21.poo_2023_01.t17;
+
+public class Computador {
+    public Mouse mouse;
+    public Teclado teclado;
+    public Monitor monitor;
+    public PlacaMae placaMae;
+}
---
+++ src/main/java/com/github/alinenunes21/poo_2023_01/t17/Memoria.java
@@ -0,0 +1,7 @@
+package com.github.alinenunes21.poo_2023_01.t17;
+
+public class Memoria {
+    public void Qnt() {
+    }
+
+}
